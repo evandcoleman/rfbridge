09084a3dec560374d7e01b621466bc2f6f0daf9b
Evan Coleman <e@edc.me>
2017-10-16 00:38:52 -0400

    fix

--- rfbridge/server.py
+++ rfbridge/server.py
@@ -11,7 +11,7 @@ _logger = logging.getLogger(__name__)
 class Server:
 
     def __init__(self, port):
-        self.server = WebsocketServer(port, host=socket.inet_aton("127.0.0.1"))
+        self.server = WebsocketServer(port, host=socket.gethostname())
         self.server.set_fn_message_received(self.message_received)
         self.server.run_forever()
 
