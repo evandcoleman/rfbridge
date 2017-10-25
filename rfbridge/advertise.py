c173cebd54883e920efb4b7b0e88e2bc9587b5ee
Evan Coleman <e@edc.me>
2017-10-25 00:18:53 -0400

    fix ip

--- rfbridge/advertise.py
+++ rfbridge/advertise.py
@@ -16,7 +16,7 @@ class Advertise:
     def get_hostname_port(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind(("",0))
-        hostname = socket.gethostname()
+        hostname = socket.gethostbyname(socket.gethostname())
         port = s.getsockname()[1]
         s.close()
         return hostname, port
