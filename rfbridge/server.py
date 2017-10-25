7d1d10ae93cc5ea98ee4b1fff039e1896bcf91f4
Evan Coleman <e@edc.me>
2017-10-25 00:44:17 -0400

    fix ip

--- rfbridge/server.py
+++ rfbridge/server.py
@@ -11,7 +11,7 @@ _logger = logging.getLogger(__name__)
 class Server:
 
     def __init__(self, port):
-        self.server = WebsocketServer(port, host=socket.gethostname())
+        self.server = WebsocketServer(port, host="0.0.0.0")
         self.server.set_fn_new_client(self.new_client)
         self.server.set_fn_message_received(self.message_received)
         self.server.run_forever()
