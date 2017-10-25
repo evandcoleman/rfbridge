e7f093b6a1b60552d2c4920453c66de23e9f9453
Evan Coleman <e@edc.me>
2017-10-25 00:21:40 -0400

    fix ip

--- rfbridge/advertise.py
+++ rfbridge/advertise.py
@@ -16,7 +16,7 @@ class Advertise:
     def get_hostname_port(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind(("",0))
-        hostname = socket.gethostbyname(socket.getfqdn())
+        hostname = socket.gethostname()
         port = s.getsockname()[1]
         s.close()
         return hostname, port
@@ -24,11 +24,11 @@ class Advertise:
     def start(self):
         hostname, port = self.get_hostname_port()
         self.port = port
-        desc = {'service': 'RF Bridge', 'version': '1.0.0'}
+        desc = {'service': 'RF Bridge', 'version': '0.0.1'}
         info = ServiceInfo(
             "_rfbridge._tcp.local.",
             hostname + "._rfbridge._tcp.local.",
-            socket.inet_aton("127.0.0.1"), port, 0, 0,
+            socket.gethostbyname(socket.getfqdn()), port, 0, 0,
             desc, hostname + ".local."
         )
         self.zeroconf.register_service(info)
